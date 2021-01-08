982534a40126d0b9fd005ae5f27cfb386b43809a
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-08 13:24:31 -0800

    initialized db folder with schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Chicken Ranch Burger', false);
