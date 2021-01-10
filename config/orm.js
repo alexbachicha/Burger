6f4d2955b7bbf87c94197c21fc1b8d2d8510de43
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-09 19:58:43 -0800

    put back the helper functions

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,28 @@
 var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objtoSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+       arr.push(key + "=" + ob[key]);
+    }
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
