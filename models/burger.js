11f25a53b46bd7384a9b04860dfbe04e19843f99
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-09 17:07:07 -0800

    added pseudocode to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,31 @@
+// Import orm.js into burger.js
 var orm = require("../config/orm.js");
 
 var burger = {
+    // Display all burgers
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
-
+    // Add a new burger
     insertOne: function(cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
-
+    // Change devoured status
     updateOne: function(objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     },
-
+    // Delete a burger
     deleteOne: function(condition, cb) {
         orm.deleteOne("burgers", condition, function(res) {
             cb(res);
         });
     }
 }
-
+// Export 
 module.exports = burger;
