03cc96a2a86da0526e120b970d332679113fd6ef
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-09 17:19:17 -0800

    added pseudocode and fixed route info

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,28 +8,30 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 // Routes
+// GET
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = {
-            burgers: data
+            burger: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
-
+// POST
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.create(req.body.burger_name, function(result) {
         res.json({ id: result.insertId });
     })
 });
-
+// PUT
 router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
-
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+    burger.update(
+        { 
+            devoured: req.body.devoured 
+        }, condition, function(result) {
         if (result.changedRows === 0) {
             return res.status(404).end();
         } else {
@@ -37,13 +39,14 @@ router.put("/api/burgers/:id", function(req, res) {
         }
     })
 });
-
+// DELETE
 router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.deleteOne(condition, function(result) {
+    burger.delete(
+        condition, function(result) {
         if (result.changedRows === 0) {
             return res.status(404).end();
         } else {
@@ -52,6 +55,7 @@ router.delete("/api/burgers/:id", function(req, res) {
     })
 });
 
+// Export
 module.exports = router;
 
 
