b959a3899283b38c24672fca29da216f705618f4
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-09 18:21:06 -0800

    cleaned up orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,5 @@
 var connection = require("../config/connection.js");
 
-// Helper function
-// function printQuestionMarks(num) {
-//     var arr = [];
-//     for(var i = 0; i < num; i++) {
-//         arr.push("?");
-//     }
-//     return arr.toString();
-// }
-
-// function objToSql(obj) {
-//     var arr = [];
-//     for (var key in ob) {
-//         var value = ob[key];
-//         if (Object.hasOwnProperty.call(ob, key)) {
-//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//                 value = "'" + value + "'" ;
-//             }
-//             arr.push(key + "=" + value)
-//         }
-//     }
-//     return arr.toString();
-// }
-
-
 var orm = {
     selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -37,12 +13,6 @@ var orm = {
 
     insertOne: function(table, value, cb) {
         var queryString = "INSERT INTO " + table + " SET ?";
-        // queryString += " ("; 
-        // queryString += cols.toString();
-        // queryString += ") ";
-        // queryString += "VALUES (";
-        // queryString += printQuestionMarks(vals.length)
-        // queryString += ") ";
         connection.query(querystring, value, function(err, result) {
             if (err) {
                 throw err;
@@ -53,10 +23,6 @@ var orm = {
 
     updateOne: function(table, condition, id, cb) {
         var queryString = "UPDATE " + table + " SET " + condition + " WHERE id = ?";
-        // queryString += " SET ";
-        // queryString += objToSql(objColVals);
-        // queryString += " WHERE ";
-        // queryString += condition;
         connection.query(queryString, id, function(err, res) {
             if (err) {
                 throw err;
@@ -66,17 +32,12 @@ var orm = {
     },
 
     // deleteOne: function(table, condition, cb) {
-    //     var queryString = "DELETE FROM " + table;
-    //     queryString += " WHERE ";
-    //     queryString += condition;
-
-    //     console.log(queryString);
-
+    //     var queryString = "DELETE FROM " + table + " SET " + condition + " WHERE id = ?";
     //     connection.query(dbQUery, function(err, res) {
     //         if (err) {
     //             throw err;
     //         }
-    //         cb(res);
+    //         cb(result);
     //     });
     // }
 
