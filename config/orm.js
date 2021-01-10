34ddaa6c13fe8955e168ba15922817a0efa7f63e
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-01-09 18:15:04 -0800

    restructed orm.js. commented out deleteOne function as well as the helper functions. not really sure if they were contributing to my webpage not loading. upon fixing these changes my webpage finally loaded

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,33 @@
-var connection = require("../config/connection");
+var connection = require("../config/connection.js");
 
+// Helper function
+// function printQuestionMarks(num) {
+//     var arr = [];
+//     for(var i = 0; i < num; i++) {
+//         arr.push("?");
+//     }
+//     return arr.toString();
+// }
 
-function printQuestionMarks(num) {
-    var arr = [];
-    for(var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
-}
-
-function objToSql(obj) {
-    var arr = [];
-    for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'" ;
-            }
-            arr.push(key + "=" + value)
-        }
-    }
-    return arr.toString();
-}
+// function objToSql(obj) {
+//     var arr = [];
+//     for (var key in ob) {
+//         var value = ob[key];
+//         if (Object.hasOwnProperty.call(ob, key)) {
+//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//                 value = "'" + value + "'" ;
+//             }
+//             arr.push(key + "=" + value)
+//         }
+//     }
+//     return arr.toString();
+// }
 
 
 var orm = {
-    selectAll: function(table, cb) {
-        var queryString = "SELECT * FROM " + table + ";";
-
-        connection.query(queryString, function(err, res) {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
             }
@@ -36,56 +35,50 @@ var orm = {
         });
     },
 
-    insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-        queryString += " ("; 
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length)
-        queryString += ") ";
-
-        console.log(queryString);
-
-        connection.query(querystring, vals, function(err, res) {
+    insertOne: function(table, value, cb) {
+        var queryString = "INSERT INTO " + table + " SET ?";
+        // queryString += " ("; 
+        // queryString += cols.toString();
+        // queryString += ") ";
+        // queryString += "VALUES (";
+        // queryString += printQuestionMarks(vals.length)
+        // queryString += ") ";
+        connection.query(querystring, value, function(err, result) {
             if (err) {
                 throw err;
             }
-            cb(res);
+            cb(result);
         });
     },
 
-    updateOne: function(table, objColVals, condition, callback) {
-        var queryString = "UPDATE " + table 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-
-        connection.query(queryString, function(err, res) {
+    updateOne: function(table, condition, id, cb) {
+        var queryString = "UPDATE " + table + " SET " + condition + " WHERE id = ?";
+        // queryString += " SET ";
+        // queryString += objToSql(objColVals);
+        // queryString += " WHERE ";
+        // queryString += condition;
+        connection.query(queryString, id, function(err, res) {
             if (err) {
                 throw err;
             }
-            cb(res);
+            cb(result);
         });
     },
 
-    deleteOne: function(table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
+    // deleteOne: function(table, condition, cb) {
+    //     var queryString = "DELETE FROM " + table;
+    //     queryString += " WHERE ";
+    //     queryString += condition;
 
-        console.log(queryString);
+    //     console.log(queryString);
 
-        connection.query(dbQUery, function(err, res) {
-            if (err) {
-                throw err;
-            }
-            cb(res);
-        });
-    }
+    //     connection.query(dbQUery, function(err, res) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         cb(res);
+    //     });
+    // }
 
 }
 
